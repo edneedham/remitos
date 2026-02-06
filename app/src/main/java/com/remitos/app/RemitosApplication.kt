2f7e8d578f5c482f8f2c4da20722bf6ad4ddc742
Edward Needham <edwardneedham123@gmail.com>
2026-02-06 11:45:38 -0300

    chore: expose repository from application

--- app/src/main/java/com/remitos/app/RemitosApplication.kt
+++ app/src/main/java/com/remitos/app/RemitosApplication.kt
@@ -1,10 +1,15 @@
 package com.remitos.app
 
 import android.app.Application
+import com.remitos.app.data.RemitosRepository
 import com.remitos.app.data.db.AppDatabase
 
 class RemitosApplication : Application() {
     val database: AppDatabase by lazy {
         AppDatabase.build(this)
+    }
+
+    val repository: RemitosRepository by lazy {
+        RemitosRepository(database)
     }
 }
