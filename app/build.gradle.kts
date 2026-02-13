8cf187ad6afeeb8ac75c06939f08d65a16afaad4
Edward Needham <edwardneedham123@gmail.com>
2026-02-13 14:41:10 -0300

    chore: bump version to 0.1.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.remitos.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10
-        versionName = "0.1.8"
+        versionCode = 11
+        versionName = "0.1.9"
     }
 
     buildTypes {
